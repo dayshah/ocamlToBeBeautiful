c78c3e86a966a77e8f73cad819b15164ad995ed0
dayshah <dhyeyhshah@gmail.com>
2023-11-09 22:52:12 -0500

    am i going to be functional...

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,6 @@
-let () = print_endline "Hello, World!"
+let hello = "hello world";;
+let hello2 = hello;;
+let hello3 = "hullo" in hello3;;
+
+let () = print_endline (let var = "hello" in var);;
+let () = print_endline(hello2);;
