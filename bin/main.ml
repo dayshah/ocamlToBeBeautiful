cc88b1024d25be01a189bcfee65c75e2d80f54dd
dayshah <dhyeyhshah@gmail.com>
2023-11-09 23:13:03 -0500

    this kinda crazy

--- bin/main.ml
+++ bin/main.ml
@@ -4,3 +4,8 @@ let hello3 = "hullo" in hello3;;
 
 let () = print_endline (let var = "hello" in var);;
 let () = print_endline(hello2);;
+
+let x = (fun x -> x+1) 1;;
+let () = print_endline (string_of_int x);;
+let add2 = (fun x -> x +. 1.);;
+let () = print_endline (string_of_float (add2 2.));;
